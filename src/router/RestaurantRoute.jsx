7627e18ceeaebe27f689e184135f65798dcd00f3
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-07-16 03:03:04 +0600

    restaurant route

---
+++ src/router/RestaurantRoute.jsx
@@ -0,0 +1,26 @@
+import React, { Children, useContext } from 'react';
+import { Navigate, useLocation } from 'react-router';
+import { AuthContext } from '../provider/Authprovider';
+import useUserRole from '../hooks/useUserRole';
+
+
+const RestaurantRoute = ({ children }) => {
+
+    const { user, loading } = useContext(AuthContext);
+    const { role, roleLoading } = useUserRole();
+    const location = useLocation();
+
+
+    if (loading || roleLoading) {
+        return <span className="loading loading-spinner loading-xl"></span>
+    }
+
+    if (!user || role !== 'admin') {
+        return <Navigate state={{ from: location.pathname }} to="/forbidden"></Navigate>
+    }
+
+    return children;
+
+};
+
+export default RestaurantRoute;
